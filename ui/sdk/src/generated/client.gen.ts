block/goose

feat(session): add session lifecycle status with archiving and PR-based inference
- Add a lifecycle status to sessions: active, archived, completed,
  superseded, pending, rejected (schema migration 16, status column with
  'active' default, exposed in SessionInfo meta)
- Archived and terminal sessions are hidden from session lists by default;
  'goose session list --archived' shows them. Pending keeps the session
  visible, re-activating clears archived_at
- New CLI: 'goose session archive [--status ...]', 'goose session unarchive',
  and 'goose session infer-status' which scans the conversation for GitHub
  PR URLs (message text and tool responses) and maps the PR state from 'gh'
  to a status (OPEN -> pending, MERGED -> completed, CLOSED -> rejected)
- New ACP method '_goose/unstable/session/set-status' with regenerated
  schema and TS SDK
- Desktop: archive action on session cards, status badge for non-active
  sessions, i18n for all 16 locales

Co-Authored-By: goose <opensource@block.xyz>

ui/sdk/src/generated/client.gen.ts
@@ -183,6 +183,7 @@ import type {
   ScheduleRecipeRequest_unstable,
   SetConfigExtensionEnabledRequest_unstable,
   SetRecipeSlashCommandRequest_unstable,
+  SetSessionStatusRequest_unstable,
   SetSessionSystemPromptRequest_unstable,
   SetToolPermissionsRequest_unstable,
   SetToolPermissionsResponse_unstable,
@@ -1148,6 +1149,12 @@ export class GooseExtClient {
     params: UnarchiveSessionRequest_unstable,
   ): Promise<void> {
     await this.conn.extMethod("_goose/unstable/session/unarchive", params);
+  }
+
+  async sessionSetStatus_unstable(
+    params: SetSessionStatusRequest_unstable,
+  ): Promise<void> {
+    await this.conn.extMethod("_goose/unstable/session/set-status", params);
   }
 
   async sourcesCreate_unstable(

vincenzopalazzo/edvige

ui/sdk/src/generated/client.gen.ts
@@ -170,6 +170,7 @@ import type {
   SessionActivityResponse_unstable,
   SetConfigExtensionEnabledRequest_unstable,
   SetRecipeSlashCommandRequest_unstable,
+  SetSessionStatusRequest_unstable,
   SetSessionSystemPromptRequest_unstable,
   SetToolPermissionsRequest_unstable,
   SetToolPermissionsResponse_unstable,
@@ -1106,6 +1107,12 @@ export class GooseExtClient {
     params: UnarchiveSessionRequest_unstable,
   ): Promise<void> {
     await this.conn.request("_goose/unstable/session/unarchive", params);
+  }
+
+  async sessionSetStatus_unstable(
+    params: SetSessionStatusRequest_unstable,
+  ): Promise<void> {
+    await this.conn.request("_goose/unstable/session/set-status", params);
   }
 
   async sourcesCreate_unstable(